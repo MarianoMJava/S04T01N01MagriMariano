92c5c20fba1f4034adf77b3cebd98b93af923052
MarianoMJava <magri.3003@gmail.com>
2023-06-14 08:14:32 +0200

    V 1.0.0

---
+++ src/main/java/cat/itacademy/barcelonactiva/Magri/Mariano/s04/t01/n01/S04T01N01MagriMariano/Controllers/HelloWorldControllers.java
@@ -0,0 +1,24 @@
+package cat.itacademy.barcelonactiva.Magri.Mariano.s04.t01.n01.S04T01N01MagriMariano.Controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorldControllers {
+
+    @GetMapping("/HelloWorld")
+    public String saluda(@RequestParam(value = "nombre", defaultValue = "UNKNOWN") String nombre) {
+        return "Hola, " + nombre + ". Estás ejecutando un proyecto Maven";
+    }
+
+    @GetMapping("/HelloWorld2")
+    public String saluda2(@PathVariable(value = "nombre", required = false) String nombre) {
+        if (nombre == null) {
+            return "Hola. Estás ejecutando un proyecto Maven";
+        } else {
+            return "Hola, " + nombre + ". Estás ejecutando un proyecto Maven";
+        }
+    }
+}
---
+++ src/main/java/cat/itacademy/barcelonactiva/Magri/Mariano/s04/t01/n01/S04T01N01MagriMariano/S04T01N01MagriMarianoApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.Magri.Mariano.s04.t01.n01.S04T01N01MagriMariano;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S04T01N01MagriMarianoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S04T01N01MagriMarianoApplication.class, args);
+	}
+
+}
